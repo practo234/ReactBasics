cd5270099a9754ffdaf9f43d9ebfcad6051756c1
practo234 <practoacc234@gmail.com>
2023-12-20 14:43:45 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const data = React.createElement("div", {}, React.createElement("h1", { id:'heading' }, "I'M shyam"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(data)
